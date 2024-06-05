f09d0a4fd758e5c87d31e94b875a8c7eb62c406a
Paul Becker <paulbecker2003@gmail.com>
2024-06-05 20:03:55 +0200

    Initial Commit

---
+++ WardrobeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WardrobeApp
+//
+//  Created by Paul Becker on 05.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WardrobeApp/WardrobeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeAppApp.swift
+//  WardrobeApp
+//
+//  Created by Paul Becker on 05.06.24.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
