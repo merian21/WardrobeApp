45c26233d9763192157c0f84229792788a5a368b
Paul Becker <paulbecker2003@gmail.com>
2024-06-05 22:00:28 +0200

    Added AddClothingItemButton

---
+++ WardrobeApp/AddClothingItemButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddClothingItemButton.swift
+//  WardrobeApp
+//
+//  Created by Paul Becker on 05.06.24.
+//
+
+import SwiftUI
+
+struct AddClothingItemButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddClothingItemButton()
+}
