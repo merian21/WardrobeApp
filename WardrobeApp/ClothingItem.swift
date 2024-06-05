9b5f657e6d728f9d89e4d2e1cefeb638d08c1fc4
Paul Becker <paulbecker2003@gmail.com>
2024-06-05 21:20:50 +0200

    Updated ClothingItem to include en enum ClothingType for specifiying the type of clothing item

--- WardrobeApp/ClothingItem.swift
+++ WardrobeApp/ClothingItem.swift
@@ -14,9 +14,18 @@ struct ClothingItem: Identifiable {
     let photo: Image
     let brandName: String
     let color: String
+    let type: ClothingType
     
     // Sample item for previews
     static var sampleItem: ClothingItem {
-        return ClothingItem(photo: Image("sweaterImage"), brandName: "SampleBrand", color: "Black")
+        return ClothingItem(photo: Image("sweaterImage"), brandName: "SampleBrand", color: "Black", type: ClothingType.sweater)
     }
+}
+
+enum ClothingType: String, CaseIterable {
+    case sweater = "Sweater"
+    case pants = "Pants"
+    case shorts = "Shorts"
+    case tShirt = "T-Shirt"
+    case shirt = "Shirt"
 }
