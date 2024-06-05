dedd6e8db0b0feac6816362870998a6e13b3b155
Paul Becker <paulbecker2003@gmail.com>
2024-06-05 21:38:10 +0200

    Added WardrobeView, which displays all ClothingItems in a Grid in a NavigationView

---
+++ WardrobeApp/WardrobeView.swift
@@ -0,0 +1,38 @@
+//
+//  WardrobeView.swift
+//  WardrobeApp
+//
+//  Created by Paul Becker on 05.06.24.
+//
+
+import SwiftUI
+
+struct WardrobeView: View {
+    // Observing the ViewModel
+    @ObservedObject var viewModel = ClothingViewModel()
+    
+    // Defining the layout of the grid
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                // Displaying clothing items in a grid
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(viewModel.clothingItems) { item in
+                        ClothingItemView(item: item)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("My Wardrobe")
+        }
+    }
+}
+
+#Preview {
+    WardrobeView()
+}
