b0c340e6ae969dc7a28b0dddfad5226fed6d3338
Paul Becker <paulbecker2003@gmail.com>
2024-06-06 18:39:16 +0200

    Changed design of AddClothingItemView

--- WardrobeApp/AddClothingItemView.swift
+++ WardrobeApp/AddClothingItemView.swift
@@ -15,61 +15,106 @@ struct AddClothingItemView: View {
     @State private var clothingType: ClothingType = .tShirt
     @ObservedObject var viewModel: ClothingViewModel
 
+    @State private var isColorPickerPresented = false
+    @State private var isTypePickerPresented = false
+
     var body: some View {
-        NavigationView {
-            Form {
-                Section() {
-                    Button(action: {
-                        // Add functionality to select a photo
-                    }) {
-                        if let selectedImage = selectedImage {
-                            Image(uiImage: selectedImage)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 350, height: 350)
-                        } else {
-                            Image("sweaterImage")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 350, height: 350)
+
+            VStack(spacing: 0) {
+                Button(action: {
+                    // Add functionality to select a photo
+                }) {
+                    if let selectedImage = selectedImage {
+                        Image(uiImage: selectedImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 350, height: 350)
+                            .cornerRadius(20)
+                    } else {
+                        Image("sweaterImage")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 350, height: 350)
+                            .cornerRadius(20)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                
+                Form() {
+                    Section() {
+                        TextField("Brand", text: $brandName)
+                    }
+                    .frame(height: 40)
+                    
+                    
+                    Section {
+                        HStack {
+                            Text("Color")
+                            Spacer()
+                            Text(color.rawValue)
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.black.opacity(0.3))
+                        }
+                        .frame(height: 40)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            isColorPickerPresented.toggle()
+                        }
+                        
+                        HStack {
+                            Text("Category")
+                            Spacer()
+                            Text(clothingType.rawValue)
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.black.opacity(0.3))
+                        }
+                        .frame(height: 40)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            isTypePickerPresented.toggle()
                         }
                     }
                 }
-                .frame(width: 350, height: 350)
-                .cornerRadius(20)
-                Section(header: Text("Details")) {
-                    TextField("Brand Name", text: $brandName)
-                    Picker("Color", selection: $color) {
-                        ForEach(ClothingColor.allCases) { color in
-                            HStack {
-                                Text(color.rawValue)
-                                Spacer()
-                                RoundedRectangle(cornerRadius: 5)
-                                    .fill(color.color)
-                                    .frame(width: 20, height: 20)
-                            }
-                            .tag(color)
-                        }
+                .formStyle(GroupedFormStyle())
+                
+                Button(action: {
+                    if let selectedImage = selectedImage {
+                        let newItem = ClothingItem(photo: Image(uiImage: selectedImage), brandName: brandName, color: color, type: clothingType)
+                        viewModel.addClothingItem(item: newItem)
                     }
-                    Picker("Type", selection: $clothingType) {
-                        ForEach(ClothingType.allCases, id: \.self) { type in
-                            Text(type.rawValue).tag(type)
-                        }
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    
+                    HStack {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white)
+                        Text(" Add Item")
+                            .font(.title)
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+
                     }
+                    .padding()
+                    .frame(width: 350, height: 60)
+                    .background(Color.black)
+                    .cornerRadius(20)
+                    .shadow(color: Color.black.opacity(0.2), radius: 1, x: 4, y: 4)
                 }
+                .padding()
             }
             .navigationBarTitle("Add Clothing Item", displayMode: .inline)
-            .navigationBarItems(leading: Button("Cancel") {
-                presentationMode.wrappedValue.dismiss()
-            }, trailing: Button("Save") {
-                if let selectedImage = selectedImage {
-                    let newItem = ClothingItem(photo: Image(uiImage: selectedImage), brandName: brandName, color: color, type: clothingType)
-                    viewModel.addClothingItem(item: newItem)
-                }
-                presentationMode.wrappedValue.dismiss()
-            })
+            .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
+            .navigationDestination(isPresented: $isColorPickerPresented) {
+                ClothingColorPickerView(selectedColor: $color)
+            }
+            .navigationDestination(isPresented: $isTypePickerPresented) {
+                ClothingTypePickerView(selectedType: $clothingType)
+            }
         }
-    }
+    
 }
 
 #Preview {
